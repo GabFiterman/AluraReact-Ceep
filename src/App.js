8f2e5b5223dd87f1d84a55bdd35a0a18bcf0aeff
Fiterman <gfiterman96@gmail.com>
2022-02-23 14:43:30 -0300

    Modulo 3/5

--- src/App.js
+++ src/App.js
@@ -1,17 +1,18 @@
 import React, { Component } from 'react';
 import ListaDeNotas from './components/ListaDeNotas';
 import FormularioCadastro from './components/FormularioCadastro';
+import "./assets/App.css";
+import "./assets/index.css"
 
 class App extends Component {
   render() {
     return (
-      <section>
+      <section className="conteudo">
         <FormularioCadastro />
         <ListaDeNotas />
       </section>
     );
   }
-
 }
 export default App;
 //react -> lib para renderização da interface gráfica
